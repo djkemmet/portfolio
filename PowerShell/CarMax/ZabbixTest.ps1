d12d7a846aa5ab327d1a87c7c0553bab85ff5f73
DJ <57572284+djkemmet@users.noreply.github.com>
2024-04-10 23:30:55 -0500

    Clear Sensitive Data

--- PowerShell/CarMax/ZabbixTest.ps1
+++ PowerShell/CarMax/ZabbixTest.ps1
@@ -5,8 +5,7 @@ $Creds = Get-Credential
 
 # Create list of OU to search for locations
 @(
-    "OU=HomeOffice,OU=CarMaxWorkstations,DC=KMX,DC=LOCAL",
-    "OU=Stores,OU=CarMaxWorkstations,DC=KMX,DC=LOCAL"
+
     ) | ForEach-Object -Process {
 
     # Get AD OUs contained in the locations listed above.
@@ -14,15 +13,15 @@ $Creds = Get-Credential
 
         # Fiter out undesirable OU Names based off first index.
         Switch($_.DistinguishedName.Split(",")[0]){
-            OU=HomeOffice { Write-Host "Found Home Office OU, Skipping..."}
-            OU=Kiosks {Write-Host "Found Kiosks Sub OU, Skipping..."}
-            OU=EnterpriseSystems { Write-Host "Found Enterprise Systems OU, Skipping"}
-            OU=Robotics {Write-Host "Found Robotics OU Skipping..."}
-            OU=Pilot { Write-Host "Found Pilot OU, Skipping..."}
-            OU=Shutdown_Script {Write-Host "Found shutdown Script OU, Skipping..."}
-            OU=7001-LAB {Write-Host "Skipping Malformed OU..."}
-            OU=VirtualDesktops {Write-Host "Found Virtual Desktops OU, Skipping..."}
-            OU=Stores {Write-Host "Found Stores OU, Skipping..."}
+            OU=
+            OU=
+            OU=
+            OU=
+            OU=
+            OU=
+            OU=
+            OU=
+            OU=
             # If none of the Above, add it to our group list that needs to be made in Zabbix
             default {Write-Host "FOUND OU: $_"; $GroupList += $_.split("=")[1]}
         }
@@ -34,7 +33,7 @@ Write-Host "FOUND THE FOLLOWING LOCATIONS IN AD:"
 Write-Host $GroupList
 
 #Great, We've Generated our list of locations, now Lets sign in to zabbix so we can check them 
-Connect-Zabbix -IPAddress 172.18.100.34 -PSCredential $Creds -noSSL
+Connect-Zabbix -IPAddress  -PSCredential $Creds -noSSL
 
 #Okay now that we're connected Lets check our list of host groups for location groups that match
 $GroupList | ForEach-Object -Process {
